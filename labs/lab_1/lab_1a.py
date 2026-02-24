819f71efe3f377767d620a7bedc46e189355222f
Polarized Gale <121917387+DhaiShah25@users.noreply.github.com>
2026-02-24 17:01:15 -0500

    Robot Speed Var & Explanation
    Added a comment explaining the purpose of a variable

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,12 +3,15 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+This is to simulate a change made on a robot.
 """
 
 def main():
     print("Hello World!")
 
     name = "Dhairya Shah" # TODO: Insert your name between the double quotes
+
+    robot_speed = 5 # m/s
 
     print(f"{name}, Welcome to the CSS course!")
 
